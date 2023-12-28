5b539e626877efed6573a9bd9ef53240a858a208
Muhammad Rafi Ali <therafiali@gmail.com>
2023-12-28 22:52:50 +0500

    Define All Backend logic here

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
-
+import sys
+from model import connector
 
 
 st.title("Chatbot with Simple Book API")
@@ -7,19 +8,24 @@ st.title("Chatbot with Simple Book API")
 USER_AVATAR = "👤"
 BOT_AVATAR = "🤖"
 
-password = st.sidebar.text_input("Enter Your Open API Key", type="password")
+password = st.sidebar.text_input("Enter Your Open API Key", type="password",placeholder="sk-j*****************************")
 
 st.sidebar.markdown('<a href="https://pk.linkedin.com/in/therafiali" target="_blank"><img src="https://content.linkedin.com/content/dam/me/business/en-us/amp/brand-site/v2/bg/LI-Bug.svg.original.svg" width="30" height="30"></a>', unsafe_allow_html=True)
+st.sidebar.write("Here is my linkedin")
 
 st.sidebar.markdown('<a href="https://github.com/therafiali" target="_blank"><img src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png" width="30" height="30"></a>', unsafe_allow_html=True)
+st.sidebar.write("Here is my Github")
 
 st.sidebar.markdown('<a href="https://twitter.com/therafiali" target="_blank"><img src="https://cdn4.iconfinder.com/data/icons/social-messaging-ui-color-shapes-2-free/128/social-twitter-square2-512.png" width="30" height="30"></a>', unsafe_allow_html=True)
+st.sidebar.write("Here is my Twitter")
 
-def welcome(name):
-    return f"Welcome to {name}"
+
 
 with st.form(key="my_form"):
-    text = st.text_area("Enter your text here", height=2)
+    text = st.text_area("Enter your text here",placeholder="How Can I Help You!")
+    
+    
+    if st.form_submit_button("Submit"):
+        result = connector(password,text)
+        st.markdown(f"<div style='background-color:#D4F1F4; padding:10px; margin-bottom: 20px; border-radius: 10px;'>{result}</div>", unsafe_allow_html=True)
 
-    if st.form_submit_button("Enter"):
-        st.write(welcome(text))
---
+++ model.py
@@ -0,0 +1,165 @@
+import requests
+import json
+from openai import OpenAI
+from openai.types.chat.chat_completion import ChatCompletionMessage, ChatCompletion
+from dotenv import load_dotenv, find_dotenv
+
+
+
+    
+
+def get_all_books():
+    """Get List of all books"""
+    response = requests.get('https://simple-books-api.glitch.me/books')
+    res_json = response.json()
+    res_str = json.dumps(res_json)
+    return res_str
+
+
+
+def get_book_id(name):
+    """Get Book Id from name"""
+    response = requests.get('https://simple-books-api.glitch.me/books')
+    res_json = response.json()
+
+    for book in res_json:
+        if book['name'] == name:
+            return book['id']
+
+
+def getprice(name):
+    """Get Book price by name"""
+    bookid = get_book_id(name)
+    response = requests.get(f"https://simple-books-api.glitch.me/books/{bookid}")
+    res_json = response.json()
+    get_Price = res_json['price']
+    res_str = json.dumps(get_Price)
+    return res_str
+
+
+def get_single_book(name):
+    """Get Single Book Details by name"""
+    bookid = get_book_id(name)
+    response = requests.get(f"https://simple-books-api.glitch.me/books/{bookid}")
+    res_json = response.json()
+    res_str = json.dumps(res_json)
+    return res_str
+
+
+def connector(key, prompt):
+    """
+    Get Api key and prompt
+    """
+
+    client = OpenAI(api_key=key)
+    
+    def run_conversation(main_request: str) -> str:
+        """
+        Step 1: send the conversation and available functions to the model
+        """
+        messages = [{"role": "user", "content": main_request}]  # user's message
+        tools = [
+            {
+                "type": "function",
+                "function": {
+                    "name": "get_all_books",
+                    "description": "Get the list of books",
+                },
+            },
+            {
+                "type": "function",
+                "function": {
+                    "name": "getprice",
+                    "description": "Get the price by its name",
+                    "parameters": {
+                        "type": "object",
+                        "properties": {
+                            "name": {
+                                "type": "string",
+                                "description": "name of book, e.g. The Russian",
+                            }},
+                        "required": ["name"],
+                    },
+                },
+            },
+            {
+                "type": "function",
+                "function": {
+                    "name": "get_single_book",
+                    "description": "Get the Whole Detail of by its name",
+                    "parameters": {
+                        "type": "object",
+                        "properties": {
+                            "name": {
+                                "type": "string",
+                                "description": "name of book, e.g. The Russian",
+                            }},
+                        "required": ["name"],
+                    },
+                },
+            },
+            
+        ]
+
+        # First Request
+        response: ChatCompletion = client.chat.completions.create(
+            model="gpt-3.5-turbo-1106",
+            messages=messages,
+            tools=tools,
+            tool_choice="auto",  # auto is default, but we'll be explicit
+        )
+        response_message: ChatCompletionMessage = response.choices[0].message
+        print("* First Response: ", dict(response_message))
+
+        tool_calls = response_message.tool_calls
+        print("* First Reponse Tool Calls: ", list(tool_calls))
+
+        # Step 2: check if the model wanted to call a function
+        if tool_calls:
+            # Step 3: call the function
+            # Note: the JSON response may not always be valid; be sure to handle errors
+            available_functions = {
+                "get_all_books": get_all_books,
+                "getprice":getprice,
+                "get_single_book":get_single_book,
+                
+            }  # only one function in this example, but you can have multiple
+
+            messages.append(response_message)  # extend conversation with assistant's reply
+
+            # Step 4: send the info for each function call and function response to the model
+            for tool_call in tool_calls:
+                function_name = tool_call.function.name
+                function_to_call = available_functions[function_name]
+                function_args = json.loads(tool_call.function.arguments)
+                if function_to_call.__name__ == "get_all_books":
+                    function_response = function_to_call()
+                    messages.append({
+                        "tool_call_id": tool_call.id,
+                        "role": "tool",
+                        "name": function_name,
+                        "content": function_response,
+                    })  # extend conversation with function response
+                else:
+                    function_response = function_to_call(
+                        name=function_args.get("name"),  # put location from prompt into location
+                    )
+                    messages.append(
+                        {
+                            "tool_call_id": tool_call.id,
+                            "role": "tool",
+                            "name": function_name,
+                            "content": function_response,
+                        }
+                    )  # extend conversation with function response
+
+            print("* Second Request Messages: ", list(messages))
+            second_response: ChatCompletion = client.chat.completions.create(
+                model="gpt-3.5-turbo-1106",
+                messages=messages,
+            )  # get a new response from the model where it can see the function response
+            print("* Second Response: ", dict(second_response))
+            return second_response.choices[0].message.content
+        
+    chat_result = run_conversation(prompt)
+    return chat_result
